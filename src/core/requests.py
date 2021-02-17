5af2bcc06964a6db557a093dfa16dc611750e9d7
kdelinx1488 <artemel@vtomske.ru>
2021-02-17 11:40:56 +0200

    fix param for app in requests.py

--- src/core/requests.py
+++ src/core/requests.py
@@ -3,15 +3,16 @@ import httpx
 
 class MakeRequest:
 
-    def __init__(self, uri, method, params, data):
+    def __init__(self, uri, method, params, data, app=None):
         self.uri = uri
         self.data = data
         self.params = params
         self.method = method
+        self.application = app
 
     async def do_request(self, app):
         async with httpx.AsyncClient(
-                app=app, http2=True, verify=False, max_redirects=3
+                app=self.application, http2=True, verify=False, max_redirects=3
         ) as client:  # type: httpx.AsyncClient
             response = await client.request(
                 self.method, self.uri, json=self.data, params=self.params
